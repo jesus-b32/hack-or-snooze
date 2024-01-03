ee8ddc71bd3b6956a90ae3bee3cbbf641bbdc3d5
jesus <149453045+jesus-b32@users.noreply.github.com>
2024-01-02 21:59:04 -0800

    read starter code and completed addStory function

---
+++ js/main.js
@@ -0,0 +1,51 @@
+"use strict";
+
+// So we don't have to keep re-finding things on page, find DOM elements once:
+
+const $body = $("body");
+
+const $storiesLoadingMsg = $("#stories-loading-msg");
+const $allStoriesList = $("#all-stories-list");
+
+const $loginForm = $("#login-form");
+const $signupForm = $("#signup-form");
+
+const $navLogin = $("#nav-login");
+const $navUserProfile = $("#nav-user-profile");
+const $navLogOut = $("#nav-logout");
+
+/** To make it easier for individual components to show just themselves, this
+ * is a useful function that hides pretty much everything on the page. After
+ * calling this, individual components can re-show just what they want.
+ */
+
+function hidePageComponents() {
+  const components = [
+    $allStoriesList,
+    $loginForm,
+    $signupForm,
+  ];
+  components.forEach(c => c.hide()); // jQuery method that hides the selected elements.This is similar to the CSS property display:none.
+  // Hidden elements will not be displayed at all (no longer affects the layout of the page).
+}
+
+/** Overall function to kick off the app. */
+
+async function start() {
+  console.debug("start");
+
+  // "Remember logged-in user" and log in, if credentials in localStorage
+  await checkForRememberedUser();
+  await getAndShowStoriesOnStart();
+
+  // if we got a logged-in user, show allStoriesList, nav logout, nav user profile and .main-nav-links; hide nav login
+  if (currentUser) updateUIOnUserLogin();
+}
+
+// Once the DOM is entirely loaded, begin the app
+
+console.warn("HEY STUDENT: This program sends many debug messages to" +
+  " the console. If you don't see the message 'start' below this, you're not" +
+  " seeing those helpful debug messages. In your browser console, click on" +
+  " menu 'Default Levels' and add Verbose");
+$(start);
